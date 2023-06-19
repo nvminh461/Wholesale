daa2ce70705f8437f15910e4f0d271a7c95866c1
nvminh461 <mn123733@gmail.com>
2023-06-19 11:30:13 +0700

    done admin send mail

--- Controller/Adminhtml/Contact/SendMail.php
+++ Controller/Adminhtml/Contact/SendMail.php
@@ -46,14 +46,15 @@ class SendMail extends \Magento\Backend\App\Action implements HttpGetActionInter
                 'email' => 'mn123733@gmail.com'
             ];
             $recipient = $contactInfo->getCustomer_email(); // Assuming you have an email field in the Contact model
-            $message = 'We are delighted that you have contacted us.';
-            $message .= "\n\nYour name: " . $contactInfo->getCustomer_name() . "\n";
-            $message .= "The product you want to purchase wholesale: " . $contactInfo->getProduct_name() . "\n";
-            $message .= "If correct, please confirm the information through this email. Thank you!!!\n";
+            $customer = $contactInfo->getCustomer_name();
+            $product = $contactInfo->getProduct_name();
             $transport = $this->transportBuilder
                 ->setTemplateIdentifier('contact_email_wholesale_template') // Replace with your email template identifier
                 ->setTemplateOptions(['area' => \Magento\Framework\App\Area::AREA_ADMINHTML, 'store' => \Magento\Store\Model\Store::DEFAULT_STORE_ID])
-                ->setTemplateVars(['message' => $message])
+                ->setTemplateVars([
+                    'customer' => $customer,
+                    'product' => $product,
+                ])
                 ->setFrom($sender)
                 ->addTo($recipient)
                 ->getTransport();
